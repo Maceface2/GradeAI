b2cd07561a284d46199595743ee5965a4a735cef
Maceface2 <143763394+Maceface2@users.noreply.github.com>
2025-03-06 11:56:24 -0500

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-
-// EXAMPLE API THAT NEXT.js PROVIDES
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ 
+    openAiKey: process.env.OPENAI_API_KEY 
+  })
 }
